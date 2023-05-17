e01ed0f5230d47ce210c82fe39a314ed3c47e215
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2023-05-17 08:55:21 +0200

    Al principios.

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-
+let Número = 0
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    basic.pause(10000)
+    Número = 9
+    for (let index = 0; index < 9; index++) {
+        basic.showNumber(Número)
+        basic.pause(200)
+        Número += -1
+    }
+    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    Número = 2
+    for (let index = 0; index < 2; index++) {
+        basic.showNumber(Número)
+        basic.pause(200)
+        Número += -1
+    }
+    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P4, 1)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(20000)
+    Número = 9
+    for (let index = 0; index < 9; index++) {
+        basic.showNumber(Número)
+        basic.pause(200)
+        Número += -1
+    }
+    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P4, 0)
+})
